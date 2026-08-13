c0eca8fb652a1bfa100155df4913c60de202446f
GUILHERME-WYS <guilherme16032008oliveira@gmail.com>
2026-08-13 01:34:48 -0300

    PRIMEIRO CÓDIGO JAVA

---
+++ Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String [] args) {
+        String aluno_1 = "Pedro";
+        String aluno_2 = "Maria";
+        System.out.println("O aluno 1: " + aluno_1);
+        System.out.println("O aluno 1: " + aluno_2);
+        System.out.println(aluno_1 + " eh casado com " + aluno_2);
+    }
+}
+
+
