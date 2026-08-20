77afc160740a0d07e6c9401da495a09ae8877f7d
GUILHERME-WYS <guilherme16032008oliveira@gmail.com>
2026-08-20 01:20:15 -0300

    revisão de fundamentos

---
+++ 2_variaveis/Main.java
@@ -0,0 +1,30 @@
+public class Main {
+    public static void main(String[] args){
+        String nome = "Savalo";
+        String sobrenome = "Horse";
+        int idade = 85;
+        double altura = 1.80;
+        boolean empregado = true;
+        String telefone = "(15) 70707-0707";
+        String endereco = "Barirro das Flores, numero 274";
+        int cep = 82765295;
+        String cidade = "Belo Campo";
+        String uf = "BH";
+        double salario_bruto = 3457.21;
+        double hora_extra = 20.00;
+        
+        System.out.println("Nome do aluno: " + nome + " " + sobrenome);
+        System.out.println("Idade: " + idade);
+        System.out.println("Altura: " + altura);
+        System.out.println("Empregado: " + empregado);
+        System.out.println("Telefone: " + telefone);
+        System.out.println("Endereço: " + endereco);
+        System.out.println("CEP: " + cep);
+        System.out.println("Cidade: " + cidade);
+        System.out.println("UF: " + uf);
+        System.out.println("Salário Bruto: " + salario_bruto);
+        System.out.println("Valor da Hora Extra: " + hora_extra);
+
+    }
+
+}
--- Variaveis/README.md
+++ Variaveis/README.md
@@ -1,6 +1,6 @@
 # Linguagem_de_Programacao
 Repositório para as aulas de programacao sobre Java
   #git config --global user.name "nome do git"
-#git config -- global user.email "colocar email"
+#git config --global user.email "colocar email"
 #git clone https://github.com/GUILHERME-WYS/linguagens_de_programacao.git
   
