d1b1c9fee6972e5b71f40510b50f469b331d55ae
GUILHERME-WYS <guilherme16032008oliveira@gmail.com>
2026-09-16 22:27:21 -0300

    segunda parte das funções

--- exercicio_pratico_02/Main.java
+++ exercicio_pratico_02/Main.java
@@ -1,5 +1,13 @@
-package exercicio_pratico_02;
-
 public class Main {
-    
+    static void analisar(String nome, int nota) {
+        if (nota >= 7)
+            System.out.println("O aluno(a) " + nome + " esta aprovado com a nota: " + nota);
+        else
+            System.out.println("O aluno(a) " + nome + " esta reprovado com a nota: " + nota);
+    }
+
+    // Corrigido: era "Sting[]", mudamos para "String[]"
+    public static void main(String[] args) {
+        analisar("Seleide", 8);
+    }
 }
